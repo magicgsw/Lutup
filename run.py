2e8759020c0c9934ff032d3e52a44217fccb7494
U188 <54225629+U188@users.noreply.github.com>
2021-03-09 14:06:02 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,71 @@
+import requests,random,time
+TG_BOT_TOKEN =''#line:11
+TG_USER_ID =''#line:12
+if  "TG_USER_ID" in os .environ and os .environ ["TG_USER_ID"]:#line:13
+        TG_BOT_TOKEN ='1698539466:AAHqZNARtVq2MJiFQIcygSxEjVDGpOJjY5k'#line:14
+        TG_USER_ID =os .environ ["TG_USER_ID"]#line:15
+        print ("Telegram 推送打开")#line:16
+def fristlogin():
+    a='bdEH5jnTfBaGhFywTMIscbf{a}w{b}i{c}{d}{e}P/w2SIZlbirzi7yJ2m'.format(a=random.randint(1, 9),b=random.randint(1, 9),c=random.choice('ABCDEFG'),d=random.choice('ABCDEFGH'),e=random.choice('ABCDEFG'))
+    ploy={
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-cn',
+        'Connection': 'keep-alive',
+        'Host': 'pwb.wubabanjia.net',
+        'Origin': 'https://web.wubabanjia.net',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E176 Safari/604.1',
+        'X-AFDAC809-9AD768A3':a}
+    try:
+        result=requests.get(url='https://pwb.wubabanjia.net/v1/fastlogin',headers=ploy).json()
+        #print(result['response']['token'])
+        return result['response']['token']
+    except Exception as e:
+        print(e)
+        telegram_bot('撸先生','失败！大概率网络不好，等待下一次邀请！')
+        exit()
+
+def register_email(token):
+    email='{}@qq.com'.format(random.randint(1000000,99999999))
+    print(email)
+    fils={
+        'platform':(None,'uj72624275'),
+        'email':(None,email),
+        'password':(None,'sdEWWEqqCEWW@!')
+          }
+    url = 'https://pwb.wubabanjia.net/v1/register/email?token={}'.format(token)
+    #print(url)
+    result = requests.post(url, files=fils)
+    #print(result.text)
+def telegram_bot(title, content):
+    print("\n")
+    tg_bot_token = TG_BOT_TOKEN
+    tg_user_id = TG_USER_ID
+    if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
+        tg_bot_token = os.environ["TG_BOT_TOKEN"]
+        tg_user_id = os.environ["TG_USER_ID"]
+    if not tg_bot_token or not tg_user_id:
+        print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
+        return
+    print("Telegram 推送开始")
+    send_data = {"chat_id": tg_user_id, "text": title +
+                 '\n\n'+content, "disable_web_page_preview": "true"}
+    response = requests.post(
+        url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data)
+    print(response.text)
+def invite(token):
+    invitecode=os.environ["INVITECODE"]
+    fils={'code':(None,invitecode)}
+    url='https://pwb.wubabanjia.net/v1/user/invite?token={}'.format(token)
+    #print(url)
+    result=requests.post(url,files=fils)
+    print(result.text)
+    telegram_bot('撸先生', '邀请完成！手动去兑换会员吧！')
+def main():
+    token = fristlogin()
+    time.sleep(5)
+    register_email(token)
+    time.sleep(5)
+    invite(token)
+if __name__=='__main__':
+    main()
